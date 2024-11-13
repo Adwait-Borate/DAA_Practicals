05c86a484f383b11e2ccbc6379535a4de64d6c2b
Adwait Borate <adwaitborate@gmail.com>
2024-11-13 16:13:48 +0530

    Code added

--- Recursive_01_KnapSack.cpp
+++ Recursive_01_KnapSack.cpp
@@ -1,27 +1,28 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-
-class Solution {
-public:
-   // function to return fractional weights
-   int O1Knapsack(vector<int>value,vector<int>weight,int W,int n) {
-      if(n==0 || W==0){
-         return 0;
-      }
-
-      if(weight[n-1]<=W){
-         return max(value[n-1]+O1Knapsack(value,weight,W-weight[n-1],n-1),O1Knapsack(value,weight,W,n-1));
-      }
-      
-         return O1Knapsack(value,weight,W,n-1);
-      
-      
+// Recursive function to solve the 0/1 Knapsack problem
+int knapSackRecursive(vector<int> &values, vector<int> &weights, int W, int n)
+{
+   if (n == 0 || W == 0)
+   {
+      return 0;
    }
-};
 
-int main() {
+   if (weights[n - 1] <= W)
+   {
+      // Return max of including or excluding the current item
+      return max(
+          values[n - 1] + knapSackRecursive(values, weights, W - weights[n - 1], n - 1),
+          knapSackRecursive(values, weights, W, n - 1));
+   }
+
+   // If weight is greater than W, exclude the item
+   return knapSackRecursive(values, weights, W, n - 1);
+}
+
+int main()
+{
    int n, W;
    cout << "Enter the number of items: ";
    cin >> n;
@@ -29,19 +30,17 @@ int main() {
    cout << "Enter the maximum weight of the knapsack: ";
    cin >> W;
 
-   vector<int> value;
-   vector<int> weight;
+   vector<int> values(n);
+   vector<int> weights(n);
 
-   for (int i = 0; i < n; i++) {
-      int val, wt;
+   for (int i = 0; i < n; i++)
+   {
       cout << "Enter value and weight for item " << i + 1 << ": ";
-      cin >> val >> wt;
-      value.emplace_back(val);
-      weight.emplace_back(wt);
+      cin >> values[i] >> weights[i];
    }
 
-   Solution obj;
-   double ans = obj.O1Knapsack(value, weight,W,n);
-   cout << "The maximum value is " << ans;
+   int maxValue = knapSackRecursive(values, weights, W, n);
+   cout << "The maximum value achievable is " << maxValue;
+
    return 0;
 }
